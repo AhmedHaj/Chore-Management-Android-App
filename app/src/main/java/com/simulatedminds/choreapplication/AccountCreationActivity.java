c2a7e05b3748ffda8e784c3d4e84a718431815ee
Hussein Sakr <hsakr011@uottawa.ca>
2017-11-20 20:37:01 -0500

    Made the button show different text if reward/password system is checked

--- app/src/main/java/com/simulatedminds/choreapplication/AccountCreationActivity.java
+++ app/src/main/java/com/simulatedminds/choreapplication/AccountCreationActivity.java
@@ -2,6 +2,7 @@ package com.simulatedminds.choreapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
@@ -15,5 +16,16 @@ public class AccountCreationActivity extends AppCompatActivity {
         ListAdapter accountToCreateAdapter = new AccountCreationAdapter(this, users);
         ListView listView = (ListView) findViewById(R.id.accountsToCreateList);
         listView.setAdapter(accountToCreateAdapter);
+        setCorrectTextOfButton();
+    }
+
+    //method to set the text of button id accountsCreationBtn
+    private void setCorrectTextOfButton(){
+        Button btn = (Button) findViewById(R.id.accountsCreationBtn);
+        if(CreateAppActivity.getPasswordSystem() || CreateAppActivity.getRewardSystem()){
+            btn.setText("Next");
+        } else {
+            btn.setText("Finish");
+        }
     }
 }
