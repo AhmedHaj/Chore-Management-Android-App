a24efe0acf275120382ec7197c4046a163b77ab9
AhmedHaj <ahmed1998nidal@gmail.com>
2017-11-24 14:04:30 -0500

    Created sliding tab view

--- app/src/main/java/com/simulatedminds/choreapplication/AboutUsActivity.java
+++ app/src/main/java/com/simulatedminds/choreapplication/AboutUsActivity.java
@@ -1,13 +1,13 @@
 package com.simulatedminds.choreapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class AboutUsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about_us);
+        setContentView(R.layout.activity_navigation_drawer);
     }
 }
--- app/src/main/java/com/simulatedminds/choreapplication/MainActivity.java
+++ app/src/main/java/com/simulatedminds/choreapplication/MainActivity.java
@@ -2,17 +2,18 @@ package com.simulatedminds.choreapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends NavigationDrawerActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navigation_drawer);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
